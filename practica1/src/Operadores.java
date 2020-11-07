2928d973ea30acd8dabe421ed5191660efadc52c
Sergio Ochoa Martinez <gnu.java.sergio@gmail.com>
2020-11-07 18:25:17 -0400

    Practica 1 Modificaciones

--- practica1/src/Operadores.java
+++ practica1/src/Operadores.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Operadores {
-    
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
         int operacion;
@@ -23,8 +23,10 @@ public class Operadores {
                     int b = 3;
                     int suma = a  + b;
                     int resta = a - b;
+                    int multi = a * b;
                     System.out.println("Suma: " + suma);
                     System.out.println("Resta: " + resta);
+                    System.out.println("Multiplicacion: " + multi);
                     break;
                 case 2:
                     System.out.println("2 - Logicas y de Comparación");
